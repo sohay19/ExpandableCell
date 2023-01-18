1b265d806933576734ed9cbaf1350b41b945bd0f
soha <sa4022@naver.com>
2023-01-18 12:37:24 +0900

    Revert "Revert "register""
    This reverts commit ae015953a302f71d9657ad83264d3157d4f6589e.

---
+++ ExpandableCell/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  ExpandableCell
+//
+//  Created by 소하 on 2023/01/17.
+//
+
+import Foundation
+
+
+enum Category:String {
+    case Family = "Family"
+    case Friend = "Friend"
+    case Coworker = "Coworker"
+}
